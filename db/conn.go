e219887c3c4140d288977ded24c43694223951b0
renancasalle <renancasalle@gmail.com>
2025-08-19 00:14:22 -0300

    feat: implement database connection function

--- db/conn.go
+++ db/conn.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"os"
+	"database/sql"
+	"fmt"
+
 )
 
 var (
@@ -11,3 +14,23 @@ var (
 	password = os.Getenv("POSTGRES_PASSWORD")
 	dbname   = os.Getenv("POSTGRES_DB")
 )
+
+func ConnectDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+	return db, nil
+}
