590011a0d8625cbd4932dcf486d2559f4de5fb6c
renancasalle <renancasalle@gmail.com>
2025-08-18 23:26:40 -0300

    add productcontroller and route for getproducts

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"golearn/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,10 +10,15 @@ func main() {
 
 	server := gin.Default()
 
+	ProductController := controller.NewProductController()
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
+
 	server.Run(":8080")
 }
